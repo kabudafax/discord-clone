d158b18afbc2034e01d829fb1ac7cff7cedaff07
lewis <lewisnie123@163.com>
2023-11-10 20:46:01 +0800

    some  modification for building

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -38,7 +38,7 @@ export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
         <div
           className={cn(
             "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-            params.serverId === id &&
+            params?.serverId === id &&
               "bg-primary/10 text-primary rounded-[16px]"
           )}
         >
